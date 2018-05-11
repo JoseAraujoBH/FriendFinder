164bc0c61f37aa194263caa2e16dd97eae37ae95
Jose Araujo <JoseAraujoBH@gmail.com>
2018-05-11 17:54:40 -0400

    pseudocode for server.js

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Friends data
+var friendsArray = [
+	{
+		name: 'Ellen Pompeo',
+		photo: 'http://http://cdn-img.instyle.com/sites/default/files/styles/622x350/public/images/2018/02/ellen-pompeo-5.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Patrick Dempsey',
+		photo: 'https://i2.wp.com/celebrityinside.com/wp-content/uploads/2014/11/Patrick-Dempsey-Favorite-Music-Band-Cars-Movies-Biography.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jessica Capshaw',
+		photo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/capshaw-jessica-1506979349.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Justin Chambers',
+		photo: 'http://sight-management.com/uploads/models/000/081/107/original/JustinChambers_cel_30.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Sandra Oh',
+		photo: 'https://twt-thumbs.washtimes.com/media/image/2017/02/01/sandra_c0-58-836-545_s885x516.jpg?534e2d1d48cce8bc8e060d8be5145f19087414e7',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Kevin McKidd',
+		photo: 'http://i0.lisimg.com/2416660/280full.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sara Ramirez',
+		photo: 'https://truecolorsfund.org/wp-content/uploads/2015/05/sara-ramirez-e1432245460153-300x300.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Jesse Williams',
+		photo: 'https://www.bet.com/celebrities/news/2018/01/26/jesse-williams-talks-about-his-new-bae/_jcr_content/image.large2x1image.dimg/__1516994458438__1516993688862/012618-celebs-jesse-williams.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sarah Drew',
+		photo: 'http://celebmafia.com/wp-content/uploads/2017/11/sarah-drew-grey-s-anatomy-300th-episode-celebration-in-la-12.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Katherine Heigl',
+		photo: 'http://www.usanetwork.com/sites/usanetwork/files/styles/769x434/public/2018/01/suits_promo_KatherineHeigl_1920x1080.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,11 @@
+// Required dependencies 
+
+// Express App
+
+// access CSS files
+
+// Parsing incoming requests
+
+// Application routes
+
+// Listening PORT
